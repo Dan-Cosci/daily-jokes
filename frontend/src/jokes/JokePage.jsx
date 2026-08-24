0fc6b139e350ccb43b93342a0c6e71c0ee025309
Dan_CoSci <allinasdaniel@gmail.com>
2026-08-24 13:55:58 +0800

    Frontend: Starting on the design

--- frontend/src/jokes/JokePage.jsx
+++ frontend/src/jokes/JokePage.jsx
@@ -1,10 +1,44 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 
 const JokePage = () => {
+  const [setup, setSetup] = useState("this is a setup");
+  const [punchline, setPunchline] = useState("this is a punchline");
+  const [explanation, setExplanation] = useState("this is the explanation");
+
+  const handleUpdate = async () => {
+    // call data base service returns data
+    let change = {
+      setup: "this is another setup",
+      punchline: "this is another punchline",
+      explanation: "this is another explanation"
+    }
+
+    setSetup(change.setup);
+    setPunchline(change.punchline);
+    setExplanation(change.explanation);
+
+  }
+
+  
   return (
     <div className="jokes">
-      hello
+      <div className="card">
+        <div className="front-card">
+          <p>{setup}</p>
+        </div>
+
+        <div className="back-card">
+          <p>{punchline}</p>
+          <div className="explanation">
+            <p>{explanation}</p>
+          </div>
+          <div className="btn-container">
+            <button className="random" onClick={handleUpdate}>Next Joke</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
